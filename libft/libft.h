efdbe1d1cc400563074a396c9425242eafb35200
johnconh <sevenballs@gmail.com>
2022-09-14 21:21:36 +0200

    quitar strlen

--- libft/libft.h
+++ libft/libft.h
@@ -6,14 +6,13 @@
 /*   By: jdasilva <jdasilva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 20:48:58 by jdasilva          #+#    #+#             */
-/*   Updated: 2022/09/14 10:00:48 by jdasilva         ###   ########.fr       */
+/*   Updated: 2022/09/14 21:21:22 by jdasilva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
 
@@ -33,7 +32,6 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlen(const char *s);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
