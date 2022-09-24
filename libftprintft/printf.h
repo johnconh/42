f74cf00a62e0b63a49d1e831a475a2a54176f837
johnconh <sevenballs@gmail.com>
2022-09-24 19:19:09 +0200

    Norminette pasado, falta Paco.

---
+++ libftprintft/printf.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdasilva <jdasilva@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/23 14:01:40 by jdasilva          #+#    #+#             */
+/*   Updated: 2022/09/24 19:17:59 by jdasilva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTF_H
+# define PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+int		ft_printf(char const *s, ...);
+int		ft_putchar(char c);
+size_t	ft_strlen(char *str);
+int		ft_putstr(char *str);
+int		ft_putunbr(unsigned int n);
+int		ft_putnbr(int n);
+int		ft_puthex(size_t arg, char format);
+#endif
