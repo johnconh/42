9ba9464bc0d7fb5bd9dfe954c118e62cc42cd69a
johnconh <sevenballs@gmail.com>
2022-10-24 17:45:54 +0200

    mejorado

--- Push_Swap/push_swap.c
+++ Push_Swap/push_swap.c
@@ -6,41 +6,36 @@
 /*   By: jdasilva <jdasilva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 17:29:03 by jdasilva          #+#    #+#             */
-/*   Updated: 2022/10/22 21:07:53 by jdasilva         ###   ########.fr       */
+/*   Updated: 2022/10/24 17:45:16 by jdasilva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-void ft_stack_nbr(char **argv, t_stack *stack)
+void	ft_stack_nbr(char *argv, t_stack *stack)
 {
 	long int	nbr;
 	int			i;
 	char		**str;
 
 	i = -1;
-	str = ft_split(argv[1], ' ');
+	str = ft_split(argv, ' ');
 	 while (str[++i])
 	{
 		nbr = ft_atoi(str[i]);
 		ft_stack_full(stack, nbr); 
-	}
+	} 
 } 
 
-void	*ft_multi_stack_nbr(int argc, char **argv, t_stack *stack)
+void	ft_multi_stack_nbr(int argc, char **argv, t_stack *stack)
 {
-	long int	nbr;
 	int			i;
 
-	i = 1;
-	while (i < argc)
-	{
-		nbr = ft_atoi(argv[i]);
-		ft_stack_full(stack, nbr);
-		i++;
-	}
-	return (0);
+	i = 0;
+	while (++i < argc)
+		ft_stack_nbr(argv[i], stack);
+
 }
 /* void ft_push_swap (t_stack **stack_a, t_stack **stack_b)
 {
@@ -72,7 +67,7 @@ int main (int argc, char **argv)
 	stack_a = ft_newpeek();
 	stack_b = ft_newpeek();
 	if (argc == 2)
-		ft_stack_nbr(argv, stack_a);
+		ft_stack_nbr(*argv, stack_a);
 	else if(argc > 2)
 		ft_multi_stack_nbr(argc, argv, stack_a);
 	if(!ft_check_doblenum_error(stack_a))
