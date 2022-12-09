dbaa8aac1f679ebb5118d85e65a89bdea7971fb5
johnconh <sevenballs@gmail.com>
2022-12-09 20:04:10 +0100

    Ok :D

--- So_long/src/get_map.c
+++ So_long/src/get_map.c
@@ -6,11 +6,28 @@
 /*   By: jdasilva <jdasilva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 19:35:48 by jdasilva          #+#    #+#             */
-/*   Updated: 2022/12/09 19:02:25 by jdasilva         ###   ########.fr       */
+/*   Updated: 2022/12/09 20:02:57 by jdasilva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/so_long.h"
+
+static void	ft_controlread(char *map)
+{
+	int		fd;
+	char	buff[42];
+	int		r;
+
+	fd = open(map, O_RDONLY);
+	r = read(fd, buff, 41);
+	if (r <= 0)
+	{
+		close(fd);
+		write(2, "Mapa vacio\n", 11);
+		exit (-1);
+	}
+	close(fd);
+}
 
 static void	ft_check_extension(const char *map, const char *ext)
 {
@@ -74,14 +91,15 @@ void	ft_getmap(char *map, t_game *game)
 	int	fd;
 
 	ft_check_extension(map, ".ber");
+	ft_controlread(map);
 	fd = open(map, O_RDONLY);
-	if (fd <= 0)
+	if (fd < 3)
 		exit (-1);
 	game->map_size = ft_map_size(fd);
 	close(fd);
 	game->map = (char **)malloc(sizeof(char *) * (game->map_size + 1));
 	fd = open(map, O_RDONLY);
-	if (fd <= 0)
+	if (fd < 3)
 		exit (-1);
 	ft_save_map(fd, game->map, game);
 	close(fd);
