b0b476d593f1f2fa1e5d795c75d671d95b4109f9
johnconh <sevenballs@gmail.com>
2023-06-19 18:19:08 +0200

    base& p

--- CPP/modulo06/ex02/Base.cpp
+++ CPP/modulo06/ex02/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: jdasilva <jdasilva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 20:28:12 by jdasilva          #+#    #+#             */
-/*   Updated: 2023/06/15 20:59:45 by jdasilva         ###   ########.fr       */
+/*   Updated: 2023/06/19 18:17:59 by jdasilva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,4 +39,30 @@ void identify(Base* p)
 		cout << "B\n";
 	else if (dynamic_cast<C*>(p) != NULL)
 		cout << "C\n";
+}
+
+void identify(Base& p)
+{
+	try
+	{
+		A& a = dynamic_cast<A&>(p);
+		cout << "A\n";
+	}catch(const std::bad_cast&)
+	{
+		try
+		{
+			B& b = dynamic_cast<B&>(p);
+			cout << "B\n";
+		}catch (const std::bad_cast&)
+		{
+			try
+			{
+				C& c = dynamic_cast<C&>(p);
+				cout << "C\n";
+			}catch (const std::bad_cast&)
+			{
+				cout << "Tipo desconocido\n";
+			}
+		}
+	}
 }
