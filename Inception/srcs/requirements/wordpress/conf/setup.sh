cb20b7ee486c9a9a8aa622aa88270efe6d03c6a9
johnconh <sevenballs@gmail.com>
2023-08-03 20:56:11 +0200

    --path

--- Inception/srcs/requirements/wordpress/conf/setup.sh
+++ Inception/srcs/requirements/wordpress/conf/setup.sh
@@ -16,6 +16,6 @@ wp core download --locale=es_ES --allow-root
 wp --path=/var/www/html  config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST} --locale=es_ES --allow-root --skip-check
 # Por ejemplo, puedes usar WP-CLI para configurar WordPress en este punto
 # Ejemplo: wp core install --url=http://mi-sitio.com --title="Mi Sitio" --admin_user=admin --admin_password=password --admin_email=info@mi-sitio.com
-wp core install --url=${DOMAIN_NAME} --title="Esto Funciona!!!" --admin_name=${WP_ADMIN_USER} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${EMAIL} --skip-email --allow-root 
+wp core install --path=/var/www/html --url=${DOMAIN_NAME} --title="Esto Funciona!!!" --admin_name=${WP_ADMIN_USER} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${EMAIL} --skip-email --allow-root 
 wp --path=/var/www/html theme install  https://downloads.wordpress.org/theme/astra.4.1.6.zip --allow-root
 wp --path=/var/www/html theme activate astra --allow-root
