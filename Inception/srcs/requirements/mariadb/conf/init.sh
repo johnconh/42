b0c41fbd0aa480570cf99625417ddcd3e1778c0d
johnconh <sevenballs@gmail.com>
2023-08-03 19:55:43 +0200

    actualizado

--- Inception/srcs/requirements/mariadb/conf/init.sh
+++ Inception/srcs/requirements/mariadb/conf/init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 #Cambia el propetario del directorio de datos de MariaDB
-chown -R mysql:mysql  /var/lib/mysql
+chown -R mysql /var/lib/mysql
+
+#Reemplazar la ubicacion del socket
+sed -i 's/^socket\s*=.*/socket=\/var\/run\/mysqld\/mysqld.sock/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 #Comenta lineas inecesarias en 50-server.cnf  para permitir conexiones externas
 sed -i "s/^bind-address\s*=.*$/#bind-address = 127.0.0.1/"  /etc/mysql/mariadb.conf.d/50-server.cnf
@@ -10,4 +13,6 @@ sed -i "s/skip-networking/#skip-networking/g" /etc/mysql/mariadb.conf.d/50-serve
 #Establece el puerto 3306
 sed -i "s/#\?port\s*=.*/port=3306/" /etc/mysql/mariadb.conf.d/50-server.cnf
 
+/etc/init.d/mysql restart
+chmod +x /tmp/create.sh
 service mysql restart
